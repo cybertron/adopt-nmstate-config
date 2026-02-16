43c44b041e35c638ba54aabada05a37212453156
Ben Nemec <bnemec@redhat.com>
2026-02-16 17:13:11 -0600

    Assign cluster.yml to role and retrieve masquerade addrs from API

--- adopt-nmstate.py
+++ adopt-nmstate.py
@@ -14,12 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Generate NodeNetworkConfigurationPolicy files for all of the machine-configs
+# that contain NMState day one configurations.
+
 # TODO:
-# - Retrieve masquerade address from API
 # - Handle "default" capture config
 # - Test if address append works with DHCP
 
 import base64
+import ipaddress
 import os
 import textwrap
 import yaml
@@ -28,13 +31,13 @@ import openshift_client as oc
 
 base64_prefix = 'data:text/plain;charset=utf-8;base64,'
 
-nncp_template = lambda hostname, updated: f'''apiVersion: nmstate.io/v1
+nncp_template = lambda hostname, updated, selector: f'''apiVersion: nmstate.io/v1
 kind: NodeNetworkConfigurationPolicy
 metadata:
   name: {hostname}-br-ex
 spec:
   nodeSelector:
-    kubernetes.io/hostname: {hostname}
+    {selector}
   desiredState:
 {updated}
 '''
@@ -43,26 +46,56 @@ def modify_config(f):
     encoded = f.contents.source[len(base64_prefix):]
     decoded = base64.b64decode(encoded).decode('utf-8')
     config = yaml.safe_load(decoded)
-    masqv4 = [{'ip': '169.254.0.2', 'prefix-length': 17}]
-    masqv6 = [{'ip': 'fd69::2', 'prefix-length': 112}]
+    networks = oc.selector('networks.operator.openshift.io').object()
+
+    cidrv4 = '169.254.0.0/17'
+    if networks.model.spec.defaultNetwork.ovnKubernetesConfig.gatewayConfig.ipv4:
+        cidrv4 = networks.model.spec.defaultNetwork.ovnKubernetesConfig.gatewayConfig.ipv4.internalMasqueradeSubnet
+    netv4 = ipaddress.ip_network(cidrv4, strict=False)
+    addrv4 = str(list(netv4.hosts())[1])
+    maskv4 = netv4.prefixlen
+
+    cidrv6 = 'fd69::/112'
+    if networks.model.spec.defaultNetwork.ovnKubernetesConfig.gatewayConfig.ipv6:
+        cidrv6 = networks.model.spec.defaultNetwork.ovnKubernetesConfig.gatewayConfig.ipv6.internalMasqueradeSubnet
+    netv6 = ipaddress.ip_network(cidrv6, strict=False)
+    addrv6 = str(list(netv6.hosts())[1])
+    maskv6= netv6.prefixlen
+
+    masqv4 = {'ip': addrv4, 'prefix-length': int(maskv4)}
+    masqv6 = {'ip': addrv6, 'prefix-length': int(maskv6)}
     for interface in config['interfaces']:
         if interface['name'] == 'br-ex' and interface['type'] == 'ovs-interface':
             if interface['ipv4']['enabled']:
+                if not 'address' in interface['ipv4']:
+                    interface['ipv4']['address'] = []
                 interface['ipv4']['address'].append(masqv4)
+            if interface['ipv6']['enabled']:
+                if not 'address' in interface['ipv6']:
+                    interface['ipv6']['address'] = []
+                interface['ipv6']['address'].append(masqv6)
     return config
 
-def create_nncp(updated, path):
+def create_nncp(updated, path, mc):
     hostname = os.path.splitext(os.path.basename(path))[0]
+    selector = f'kubernetes.io/hostname: {hostname}'
+    # The magic hostname of 'cluster' means to apply the config to every node in the role
+    if hostname == 'cluster':
+        role = mc.model.metadata.labels['machineconfiguration.openshift.io/role']
+        selector = f'node-role.kubernetes.io/{role}: ""'
+        hostname = role
     print(f'{hostname}.yaml')
     print('-' * (len(hostname) + 5))
-    print(nncp_template(hostname, textwrap.indent(yaml.dump(updated), '    ')))
+    print(nncp_template(hostname, textwrap.indent(yaml.dump(updated), '    '), selector))
 
 with oc.project('openshift-machine-config-operator'):
     for mc in oc.selector('machineconfigs').objects():
+        if mc.name().startswith('rendered'):
+            continue
         for f in mc.model.spec.config.storage.files:
             if f.path.startswith('/etc/nmstate/openshift'):
                 if f.contents.source.startswith(base64_prefix):
                     updated = modify_config(f)
-                    create_nncp(updated, f.path)
+                    create_nncp(updated, f.path, mc)
 
 
